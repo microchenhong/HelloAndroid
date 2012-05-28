c45609d036bf9226b81aed2638445d4c102e3c5f
Charley Chen <microchenhong@gmail.com>
2012-05-28 09:48:16 +0800

    grid layout

--- HelloAndroid/HelloViews/src/com/isoftpark/helloviews/ImageAdapter.java
+++ HelloAndroid/HelloViews/src/com/isoftpark/helloviews/ImageAdapter.java
@@ -48,7 +48,7 @@ public class ImageAdapter extends BaseAdapter {
 		ImageView imageView;
 		if (convertView == null) {
 			imageView = new ImageView(mContext);
-			imageView.setLayoutParams(new GridView.LayoutParams(35, 35));
+			imageView.setLayoutParams(new GridView.LayoutParams(85, 85));
 			imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
 			imageView.setPadding(8, 8, 8, 8);
 		}
